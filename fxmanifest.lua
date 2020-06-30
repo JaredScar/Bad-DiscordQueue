36fb481bf002f146ac3e6247ff37e260496b0e43
MiamiScripts <67082596+MiamiScripts@users.noreply.github.com>
2020-06-29 20:52:03 -0400

    Update and rename __resource.lua to fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,4 +1,9 @@
-resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'Bad-DiscordQueue'
+
 
 client_script "config.lua"
 client_script "client.lua"
