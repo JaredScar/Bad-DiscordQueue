b25644f935c1f5a145ffd9e1aef717513e79f1af
rjmxtechnology <rjmxtechnology@users.noreply.github.com>
2020-06-29 20:16:15 -0500

    Add link and author info

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,8 +2,8 @@ fx_version 'bodacious'
 
 game 'gta5'
 
-description 'Bad-DiscordQueue'
-
+description 'Bad-DiscordQueue by OfficialBadger(cfx.re)/JaredScar(github) originally released on cfx.re forums for FiveM'
+cfxlink 'https://forum.cfx.re/t/release-bad-discordqueue-a-discord-role-based-queue-system-by-badger/1394685'
 
 client_scripts { 
   "config.lua",
