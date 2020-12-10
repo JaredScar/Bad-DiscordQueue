f64b5555a86f9a195ed5c138f464fca7ef29e638
Jared <TheWolfBadger@gmail.com>
2020-12-10 17:02:31 -0500

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,7 +6,6 @@ description 'Bad-DiscordQueue by OfficialBadger(cfx.re)/JaredScar(github) origin
 cfxlink 'https://forum.cfx.re/t/release-bad-discordqueue-a-discord-role-based-queue-system-by-badger/1394685'
 
 client_scripts { 
-  "config.lua",
   "client.lua"
 }
 
